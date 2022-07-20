b88d9e3c5306d2332bcb2fa867a14b962392fc0f
박서정 <12211197@inha.edu>
2022-07-20 13:56:00 +0900

    feat: 공용컴포넌트 개발

---
+++ src/color/index.jsx
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    backgroundcolor: "",
+    main: "#FFC74A",
+    orange: "#FFA751",
+    yellow: "#FFE259",
+    black: "#000000",
+    green: "#34BA05",
+    white: "#FFFFFF",
+  },
+};
+//carrier/src/assets/theme/index.jsx 참고!!
+// 색상을 가져다 쓰는 것임, 색상 지정해주는 것임 가져다 쓸 때에는 
+//background-color: ${(props) => props.theme.colors.primary};와 같이 
+//primary 부분에 orange, yellow, black 등등 가져다 쓰면 됩니다
+//?? backgroundcolor는 그라데이션이라서 구현아직 못했어요
---
+++ src/popit-ui/Button/index.jsx
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+//노란색버튼 (칠해져 있는 것)& 겉만 노랑(아이디 ,비번 적는 곳)
+const Button = styled.div`
+  position: relative;
+  background-color: ${(props) => props.theme.colors.main};
+  border-radius: 52px;
+  heigth: 48px;
+  width: 324px;
+  border: ${(props) => (props.border ? props.border : "none")};
+  color: white;
+  font-size: 16px;
+  text-align: center;
+
+  &:hover {
+    background-color: gray;
+  }
+
+  ${(props) =>
+    props.medium &&
+    css`
+      width: 160px;
+    `};
+
+  ${(props) =>
+    props.small &&
+    css`
+      width: 70px;
+      height: 30px;
+    `};
+`;
+export default Button;
+//props.medium은 로그인 버튼, props.small은 main에서 완료버튼입니다
+//figma 형식 그대로 만들긴 했는데 height부분은 수정해야 할 것 같습니다.
+//hover  background color 일단 gray로 해놨어요
+//수정사항 있으면 고쳐주세요 !
