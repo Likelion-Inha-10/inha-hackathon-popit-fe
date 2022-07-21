d8db1457623e6c8a2b216d72eec17ae73897e633
pakxe <pigkill40@naver.com>
2022-07-21 17:21:34 +0900

    feat: FirstHeader 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import Layout from "./popit-ui/Layout";
 import Typography from "./popit-ui/Typography";
 import Margin from "./popit-ui/Margin";
 import RoundBox from "./popit-ui/RoundBox";
-import Header from "./popit-ui/Header";
+import FirstHeader from "./popit-ui/FirstHeader";
 import { IoChevronBackSharp } from "react-icons/io5";
 
 function App() {
   return (
     <>
       <Layout gradation down>
-        <Header
-          firstComponent={<IoChevronBackSharp />}
-          secondComponent={<h1>POPIT</h1>}
-          thirdComponent={<h1>3</h1>}
-        ></Header>
-        <h1>나는 천재</h1>
+        <FirstHeader back popit></FirstHeader>
 
         <RoundBox white down height="785px"></RoundBox>
       </Layout>
