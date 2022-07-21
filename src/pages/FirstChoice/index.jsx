0eefd844484a2d1ce09baa2c471957f92dc1fcf6
pakxe <pigkill40@naver.com>
2022-07-22 04:20:53 +0900

    chore: first-choice 페이지 타이틀 텍스트 추가

--- src/pages/FirstChoice/index.jsx
+++ src/pages/FirstChoice/index.jsx
@@ -2,12 +2,28 @@ import Layout from "../../popit-ui/Layout";
 import FirstHeader from "../../popit-ui/FirstHeader";
 import RoundBox from "../../popit-ui/RoundBox";
 import styled from "styled-components";
+import Flex from "../../popit-ui/Flex";
+import Typography from "../../popit-ui/Typography";
+import Margin from "../../popit-ui/Margin";
 
+const Wrapper = styled(Flex)``;
 const FirstChoice = () => {
   return (
     <Layout gradation down>
       <FirstHeader back popit />
-      <RoundBox white down height="785px"></RoundBox>
+      <RoundBox white down height="785px">
+        <Wrapper direction="column" align="center">
+          <Typography headline bold300>
+            팝 보관 카테고리 3개 선택
+          </Typography>
+          <Margin height="8px" />
+          <Typography center color="darkgray" size="12px">
+            보관하고 싶은 공부 분야를 선택하여
+            <br />
+            나에게 맞는 팝들을 받아보세요.
+          </Typography>
+        </Wrapper>
+      </RoundBox>
     </Layout>
   );
 };
