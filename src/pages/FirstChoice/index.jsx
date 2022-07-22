ad354993d63b9d8e5afbb3d37f6596dcf22d0d1d
pakxe <pigkill40@naver.com>
2022-07-22 21:56:45 +0900

    feat: first-choice 페이지 추가

--- src/pages/FirstChoice/index.jsx
+++ src/pages/FirstChoice/index.jsx
@@ -5,23 +5,28 @@ import styled from "styled-components";
 import Flex from "../../popit-ui/Flex";
 import Typography from "../../popit-ui/Typography";
 import Margin from "../../popit-ui/Margin";
+import CategoryList from "./components";
 
-const Wrapper = styled(Flex)``;
+const Wrapper = styled(Flex)`
+  padding: 50px 30px 0 30px;
+`;
+
 const FirstChoice = () => {
   return (
     <Layout gradation down>
       <FirstHeader back popit />
-      <RoundBox white down height="785px">
+      <RoundBox white down height="775px">
         <Wrapper direction="column" align="center">
-          <Typography headline bold300>
-            팝 보관 카테고리 3개 선택
-          </Typography>
-          <Margin height="8px" />
+          <Typography headline>팝 보관 카테고리 3개 선택</Typography>
+          <Margin height="10px" />
           <Typography small center color="darkgray">
             보관하고 싶은 공부 분야를 선택하여
             <br />
             나에게 맞는 팝들을 받아보세요.
           </Typography>
+          <Margin height="45px" />
+
+          <CategoryList />
         </Wrapper>
       </RoundBox>
     </Layout>
