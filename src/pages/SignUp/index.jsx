3d1b839fa3cf04f265a273ff36f27fa7737cc37c
eunseo <eundeok9@gmail.com>
2022-07-23 20:13:14 +0900

    feat: 입력, 클릭 이벤트 추가

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -8,52 +8,82 @@ import Flex from "../../popit-ui/Flex";
 import Layout from "../../popit-ui/Layout";
 import FirstHeader from "./../../popit-ui/FirstHeader/index";
 import Input from "../../popit-ui/Input";
+import Alert from "../../popit-ui/Alert";
 
 const SignUp = () => {
   const navigate = useNavigate();
-
   const [userName, setName] = useState("");
-  const [userId, setId] = useState("");
+  const [userEmail, setEmail] = useState("");
   const [userPassword, setPassword] = useState("");
   const [userPasswordCheck, setPasswordCheck] = useState("");
 
-  // const handleName = (e) => {
-  //   console.log(e.target.value);
-  // };
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
 
-  const onClick = () => {
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onChangePasswordCheck = (e) => {
+    setPasswordCheck(e.target.value);
+  };
+
+  const onClickBack = () => {
     navigate(-1);
+  };
+
+  const onClickSubmit = () => {
+    if (!userName || !userEmail || !userPassword || !userPasswordCheck) {
+      Alert("모든 항목을 입력해주세요.");
+    } else if (userPassword !== userPasswordCheck) {
+      Alert("비밀번호를 확인해주세요.");
+    } else {
+      //api 연동 필요
+      navigate("/first-choice");
+    }
   };
 
   return (
     <>
       <Layout gradation>
-        <FirstHeader onClick={onClick} popit back />
-        <RoundBox down height="775px" white>
+        <FirstHeader onClick={onClickBack} popit back />
+        <RoundBox down height="780px" white>
           <Flex direction="column" align="center">
             <Margin height="160px" />
             <Typography color="gray" center title underline>
               회원가입
             </Typography>
             <Margin height="40px" />
-            <Input type="text" placeholder="이름을 입력해주세요." />
+            <Input
+              onChange={onChangeName}
+              type="text"
+              placeholder="이름을 입력해주세요."
+            />
             <Margin height="20px" />
             <Input
+              onChange={onChangeEmail}
               type="email"
               placeholder="사용하실 이메일(아이디)을 입력해주세요."
             />
             <Margin height="20px" />
             <Input
+              onChange={onChangePassword}
               type="password"
               placeholder="사용하실 비밀번호를 입력해주세요."
             />
             <Margin height="20px" />
             <Input
+              onChange={onChangePasswordCheck}
               type="password"
               placeholder="비밀번호를 한 번 더 입력해주세요."
             />
-            <Margin height="30px" width="100%" />
-            <Button>가입하기</Button>
+            <Margin height="30px" />
+            <Button onClick={onClickSubmit}>가입하기</Button>
           </Flex>
         </RoundBox>
       </Layout>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { IoFileTrayStacked } from "react-icons/io5";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Intro from "../pages/Intro";
 import FirstChoice from "../pages/FirstChoice";
+import SignUp from "../pages/SignUp";
 
 // 라우트명은 kebab-case 로 작성합니다
 
@@ -10,6 +11,7 @@ const Router = () => (
     <Routes>
       <Route exact path="/" element={<Intro />}></Route>
       <Route exact path="/first-choice" element={<FirstChoice />}></Route>
+      <Route esact path="/sign-up" element={<SignUp />} />
     </Routes>
   </BrowserRouter>
 );
