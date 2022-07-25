1e161a2f5e3698d1c14006d339f27054dbed624c
pakxe <pigkill40@naver.com>
2022-07-25 19:58:25 +0900

    chore: 코드 수정

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -5,25 +5,7 @@ import Layout from "../../popit-ui/Layout";
 const Main = () => {
   return (
     <Layout white>
-      <Button whiteGray redTypo>
-        팔로우 취소
-      </Button>
-
-      <Button whiteGray>팝 저장</Button>
-
-      <Button large>hi</Button>
-
-      <Button medium>확인</Button>
-
-      <Button medium white>
-        취소
-      </Button>
-
-      <Button small>하이</Button>
-
-      <Button extraSmall gray>
-        삭제
-      </Button>
+      
     </Layout>
   );
 };
