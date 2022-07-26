8f70fc851ce4d09290aa87f0c2c8920854441cb4
pakxe <pigkill40@naver.com>
2022-07-26 16:39:23 +0900

    feat: Pop 컴포넌트 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import Layout from "./../../popit-ui/Layout";
-import Inputpopup from "./../../popit-ui/Inputpopup/index";
-import Categoryicon from "../../popit-ui/Categoryicon";
-import dog from "../../popit-ui/Categoryicon/dog.png";
+import Layout from "../../popit-ui/Layout";
+import ToggleButton from "../../popit-ui/ToggleButton";
+import { useState } from "react";
+import Pop from "../../popit-ui/Pop";
+import cat from "../../assets/image/babo-cat.png";
+
 const Main = () => {
+  const [toggled, setToggled] = useState(false);
   return (
     <Layout white>
-      <Categoryicon image={dog}></Categoryicon>
+      <ToggleButton onChange={(event) => setToggled(event.target.checked)} />
+      <ToggleButton onChange={(event) => setToggled(event.target.checked)} />
+      <ToggleButton onChange={(event) => setToggled(event.target.checked)} />
+      <ToggleButton onChange={(event) => setToggled(event.target.checked)} />
+      <p>The switch is {toggled ? "on" : "off"}</p>
+      <Pop src={cat} repls={1} hearts={24} small>
+        daskfhaksdfh
+      </Pop>
+      <Pop src={cat} repls={2934} hearts={232}>
+        Html은 프로그래밍 언어가 아니다
+      </Pop>
     </Layout>
   );
 };
