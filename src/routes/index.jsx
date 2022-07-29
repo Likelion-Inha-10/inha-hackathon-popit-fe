4c1bede5a31de32b89451f52919ab78576a2f331
pakxe <pigkill40@naver.com>
2022-07-29 17:27:48 +0900

    chore: host 주소 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -12,7 +12,7 @@ import Test from "../pages/Test";
 import MyChatList from "./../pages/MyChatList";
 import ProfileEdit from "./../pages/ProfileEdit/index";
 import MorePop from "../pages/MorePop";
-import InputPopUp from "./../popit-ui/InputPopUp/index";
+import InputPopUp from "../popit-ui/InputPopUp/index";
 import FollowerList from "../pages/FollowerList";
 import FollowingList from "../pages/FollowingList";
 import ReplList from "../pages/ReplList";
@@ -21,28 +21,57 @@ import CategoryPopList from "../pages/CategoryPopList";
 import MyPopListEdit from "../pages/MyPopListEdit";
 // 라우트명은 kebab-case 로 작성합니다
 
+const API_URL = "http://172.105.201.195:8000/";
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Intro />} />
-      <Route exact path="/tutorial" element={<Tutorial />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/sign-up" element={<SignUp />} />
-      <Route exact path="/first-choice" element={<FirstChoice />} />
-      <Route exact path="/main" element={<Main />} />
-      <Route exact path="/menu" element={<Hamburger />} />
-      <Route exact path="/owner-profile" element={<OwnerProfile />} />
-      <Route exact path="/test" element={<Test />} />
-      <Route exact path="/mychat-list" element={<MyChatList />} />
-      <Route exact path="/profile-edit" element={<ProfileEdit />} />
-      <Route exact path="/more-pop" element={<MorePop />} />
-      <Route exact path="/inputpop" element={<InputPopUp />} />
-      <Route exact path="/follower-list" element={<FollowerList />} />
-      <Route exact path="/following-list" element={<FollowingList />} />
-      <Route exact path="/repl-list" element={<ReplList />} />
-      <Route exact path="/pop-list" element={<MyPopList />} />
-      <Route exact path="/pop-list/edit" element={<MyPopListEdit />} />
-      <Route exact path="/pop-list/:title" element={<CategoryPopList />} />
+      <Route exact path="/" element={<Intro api={API_URL} />} />
+      <Route exact path="/tutorial" element={<Tutorial api={API_URL} />} />
+      <Route exact path="/login" element={<Login api={API_URL} />} />
+      <Route exact path="/sign-up" element={<SignUp api={API_URL} />} />
+      <Route
+        exact
+        path="/first-choice"
+        element={<FirstChoice api={API_URL} />}
+      />
+      <Route exact path="/main" element={<Main api={API_URL} />} />
+      <Route exact path="/menu" element={<Hamburger api={API_URL} />} />
+      <Route
+        exact
+        path="/owner-profile"
+        element={<OwnerProfile api={API_URL} />}
+      />
+      <Route exact path="/test" element={<Test api={API_URL} />} />
+      <Route exact path="/mychat-list" element={<MyChatList api={API_URL} />} />
+      <Route
+        exact
+        path="/profile-edit"
+        element={<ProfileEdit api={API_URL} />}
+      />
+      <Route exact path="/more-pop" element={<MorePop api={API_URL} />} />
+      <Route exact path="/inputpop" element={<InputPopUp api={API_URL} />} />
+      <Route
+        exact
+        path="/follower-list"
+        element={<FollowerList api={API_URL} />}
+      />
+      <Route
+        exact
+        path="/following-list"
+        element={<FollowingList api={API_URL} />}
+      />
+      <Route exact path="/repl-list" element={<ReplList api={API_URL} />} />
+      <Route exact path="/pop-list" element={<MyPopList api={API_URL} />} />
+      <Route
+        exact
+        path="/pop-list/edit"
+        element={<MyPopListEdit api={API_URL} />}
+      />
+      <Route
+        exact
+        path="/pop-list/:title"
+        element={<CategoryPopList api={API_URL} />}
+      />
     </Routes>
   </BrowserRouter>
 );
