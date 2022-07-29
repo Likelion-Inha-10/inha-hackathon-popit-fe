7665097e06a9a10464979400220bb9380020abe2
pakxe <pigkill40@naver.com>
2022-07-30 05:40:11 +0900

    chore: 더이상 못하겠어git add .

--- src/pages/Main/components/CategoryList/index.jsx
+++ src/pages/Main/components/CategoryList/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "../../../../popit-ui/Typography";
 import Margin from "../../../../popit-ui/Margin";
 import ProfileIcon from "../../../../popit-ui/ProfileIcon/index";
@@ -6,6 +6,8 @@ import styled from "styled-components";
 import cat from "../../../../assets/image/babo-cat.png";
 import Flex from "../../../../popit-ui/Flex";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Alert from "../../../../popit-ui/Alert";
 
 const Category = styled(Flex)`
   cursor: pointer;
@@ -20,18 +22,42 @@ const CategoryContainer = styled(Flex)``;
 
 const CategoryList = (props) => {
   const navigate = useNavigate();
+  const [allCategory, setAllCategory] = useState([]);
 
-  const moveToCategory = () => {};
+  const moveToCategory = () => {
+    Alert("착한사람한테만 보이는 창");
+  };
   return (
     <CategoryWrapper>
       <CategoryContainer>
         <Category align="center" direction="column" onClick={moveToCategory}>
-          <ProfileIcon size48 backImage={cat} />
-          <Margin height="7px" />
-          <Typography regular12>화학</Typography>
+          <ProfileIcon
+            size48
+            backImage="http://172.105.201.195:8000/media/ch.jpg"
+          />
+          <Margin height="10px" />
+          <Typography regular12>IT</Typography>
         </Category>
         <Margin width="10px" />
-      </CategoryContainer>{" "}
+        <Category align="center" direction="column" onClick={moveToCategory}>
+          <ProfileIcon
+            size48
+            backImage="http://172.105.201.195:8000/media/Sports.jpg"
+          />
+          <Margin height="10px" />
+          <Typography regular12>스포츠</Typography>
+        </Category>
+        <Margin width="10px" />
+        <Category align="center" direction="column" onClick={moveToCategory}>
+          <ProfileIcon
+            size48
+            backImage="http://172.105.201.195:8000/media/Health.png"
+          />
+          <Margin height="10px" />
+          <Typography regular12>Health</Typography>
+        </Category>
+        <Margin width="10px" />
+      </CategoryContainer>
       {/* <CategoryContainer>
         <Category align="center" direction="column" onClick={moveToCategory}>
           <ProfileIcon size48 backImage={cat} />
