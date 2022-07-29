88816e004814b7ca45197f161639849fa1e1ed5e
pakxe <pigkill40@naver.com>
2022-07-29 17:41:57 +0900

    chore: .env 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -21,56 +21,103 @@ import CategoryPopList from "../pages/CategoryPopList";
 import MyPopListEdit from "../pages/MyPopListEdit";
 // 라우트명은 kebab-case 로 작성합니다
 
-const API_URL = "http://172.105.201.195:8000/";
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Intro api={API_URL} />} />
-      <Route exact path="/tutorial" element={<Tutorial api={API_URL} />} />
-      <Route exact path="/login" element={<Login api={API_URL} />} />
-      <Route exact path="/sign-up" element={<SignUp api={API_URL} />} />
+      <Route
+        exact
+        path="/"
+        element={<Intro api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/tutorial"
+        element={<Tutorial api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/login"
+        element={<Login api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/sign-up"
+        element={<SignUp api={process.env.REACT_APP_API} />}
+      />
       <Route
         exact
         path="/first-choice"
-        element={<FirstChoice api={API_URL} />}
+        element={<FirstChoice api={process.env.REACT_APP_API} />}
       />
-      <Route exact path="/main" element={<Main api={API_URL} />} />
-      <Route exact path="/menu" element={<Hamburger api={API_URL} />} />
+      <Route
+        exact
+        path="/main"
+        element={<Main api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/menu"
+        element={<Hamburger api={process.env.REACT_APP_API} />}
+      />
       <Route
         exact
         path="/owner-profile"
-        element={<OwnerProfile api={API_URL} />}
+        element={<OwnerProfile api={process.env.REACT_APP_API} />}
       />
-      <Route exact path="/test" element={<Test api={API_URL} />} />
-      <Route exact path="/mychat-list" element={<MyChatList api={API_URL} />} />
+      <Route
+        exact
+        path="/test"
+        element={<Test api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/mychat-list"
+        element={<MyChatList api={process.env.REACT_APP_API} />}
+      />
       <Route
         exact
         path="/profile-edit"
-        element={<ProfileEdit api={API_URL} />}
+        element={<ProfileEdit api={process.env.REACT_APP_API} />}
       />
-      <Route exact path="/more-pop" element={<MorePop api={API_URL} />} />
-      <Route exact path="/inputpop" element={<InputPopUp api={API_URL} />} />
+      <Route
+        exact
+        path="/more-pop"
+        element={<MorePop api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/inputpop"
+        element={<InputPopUp api={process.env.REACT_APP_API} />}
+      />
       <Route
         exact
         path="/follower-list"
-        element={<FollowerList api={API_URL} />}
+        element={<FollowerList api={process.env.REACT_APP_API} />}
       />
       <Route
         exact
         path="/following-list"
-        element={<FollowingList api={API_URL} />}
+        element={<FollowingList api={process.env.REACT_APP_API} />}
       />
-      <Route exact path="/repl-list" element={<ReplList api={API_URL} />} />
-      <Route exact path="/pop-list" element={<MyPopList api={API_URL} />} />
+      <Route
+        exact
+        path="/repl-list"
+        element={<ReplList api={process.env.REACT_APP_API} />}
+      />
+      <Route
+        exact
+        path="/pop-list"
+        element={<MyPopList api={process.env.REACT_APP_API} />}
+      />
       <Route
         exact
         path="/pop-list/edit"
-        element={<MyPopListEdit api={API_URL} />}
+        element={<MyPopListEdit api={process.env.REACT_APP_API} />}
       />
       <Route
         exact
         path="/pop-list/:title"
-        element={<CategoryPopList api={API_URL} />}
+        element={<CategoryPopList api={process.env.REACT_APP_API} />}
       />
     </Routes>
   </BrowserRouter>
